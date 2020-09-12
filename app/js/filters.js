14f3fd6fb20e91774ab34d5461aac9f820b667d3
cgorsko <cgorski@stevensgroupweb.com>
2020-09-12 10:01:49 -0500

    Add shipperFilter function

--- app/js/filters.js
+++ app/js/filters.js
@@ -78,3 +78,25 @@ four51.app.filter('paginate', function() {
 		return input.slice(start);
 	}
 });
+
+four51.app.filter('shipperFilter', function() {
+    return function(shipper) {
+        var results = [];
+        var freight = [];
+        angular.forEach(shipper, function(s) {
+            if(s.Name.indexOf('NAMEOFSHIPPERGOESHERE') != -1){
+                freight.push(s);
+            }
+            else{
+                results.push(s);
+            }
+        });
+        if(results.length > 0){
+            return results;
+        }
+        else{
+            return freight;
+        }
+    }
+});
+
