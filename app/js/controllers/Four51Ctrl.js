7f84452b65cb0ba551c72713be9e3adb7fdd5fb5
cgorsko <cgorski@stevensgroupweb.com>
2021-01-10 08:51:05 -0600

    Added Four51 Code to Timeout Login after 15min

--- app/js/controllers/Four51Ctrl.js
+++ app/js/controllers/Four51Ctrl.js
@@ -1,5 +1,5 @@
-four51.app.controller('Four51Ctrl', ['$scope', '$route', '$location', '$451', 'User', 'Order', 'Security', 'OrderConfig', 'Category', 'AppConst','XLATService', 'GoogleAnalytics', 'SpendingAccount',
-function ($scope, $route, $location, $451, User, Order, Security, OrderConfig, Category, AppConst, XLATService, GoogleAnalytics, SpendingAccount) {
+four51.app.controller('Four51Ctrl', ['$scope', '$route', '$rootScope', '$timeout', '$document', '$window', '$location', '$451', 'User', 'Order', 'Security', 'OrderConfig', 'Category', 'AppConst','XLATService', 'GoogleAnalytics', 'SpendingAccount',
+function ($scope, $route, $rootScope, $timeout, $document, $window,  $location, $451, User, Order, Security, OrderConfig, Category, AppConst, XLATService, GoogleAnalytics, SpendingAccount) {
 	$scope.AppConst = AppConst;
 	$scope.scroll = 0;
 	$scope.isAnon = $451.isAnon; //need to know this before we have access to the user object
@@ -75,4 +75,37 @@ function ($scope, $route, $location, $451, User, Order, Security, OrderConfig, C
 	});
 	$scope.$on("$routeChangeSuccess", init);
     $scope.$on('event:auth-loginRequired', cleanup);
+	
+	// Timeout timer value
+	var TimeOutTimerValue = 15*60*1000;
+
+	// Start a timeout
+	var TimeOut_Thread = $timeout(function(){ LogoutByTimer() } , TimeOutTimerValue);
+	var bodyElement = angular.element($document);
+
+	angular.forEach(['keydown', 'keyup', 'click', 'mousemove', 'DOMMouseScroll', 'mousewheel', 'mousedown', 'touchstart', 'touchmove', 'scroll', 'focus'],
+		function(EventName) {
+			bodyElement.bind(EventName, function (e) { TimeOut_Resetter(e) });
+		});
+
+	function LogoutByTimer(){
+	    User.logout($scope.user, function(u){
+        	if ($scope.isAnon) {
+        	    $timeout(function () {
+                    $location.path("/catalog");
+	                location.reload(true);
+                }, 500);
+            }
+        }, function(ex){
+        	console.log(ex.Message);
+        });
+	}
+
+	function TimeOut_Resetter(e){
+		/// Stop the pending timeout
+		$timeout.cancel(TimeOut_Thread);
+
+		/// Reset the timeout
+		TimeOut_Thread = $timeout(function(){ LogoutByTimer() } , TimeOutTimerValue);
+	}
 }]);
